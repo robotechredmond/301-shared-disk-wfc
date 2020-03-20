266e24d4e2c71d1325457b8cdeb69229927b1ef8
Keith Mayer <keith@keithmayer.com>
2020-03-20 11:54:48 -0400

    fixed format shared volumes setscript

--- dsc/ConfigureCluster.ps1/ConfigureCluster.ps1
+++ dsc/ConfigureCluster.ps1/ConfigureCluster.ps1
@@ -133,7 +133,7 @@ configuration ConfigureCluster
 
         Script FormatSharedDisks
         {
-            SetScript = "Get-Disk | Where-Object PartitionStyle -eq 'RAW' | Initialize-Disk -PartitionStyle GPT -PassThru -ErrorAction SilentlyContinue | New-Partition -AssignDriveLetter -UseMaximumSize -ErrorAction SilentlyContinue | Format-Volume -FileSystem NTFS -Confirm:${false}"
+            SetScript = "Get-Disk | Where-Object PartitionStyle -eq 'RAW' | Initialize-Disk -PartitionStyle GPT -PassThru -ErrorAction SilentlyContinue | New-Partition -AssignDriveLetter -UseMaximumSize -ErrorAction SilentlyContinue | Format-Volume -FileSystem NTFS -Confirm:`$false"
             TestScript = "(Get-Disk | Where-Object PartitionStyle -eq 'RAW').Count -eq 0"
             GetScript = "@{Ensure = if ((Get-Disk | Where-Object PartitionStyle -eq 'RAW').Count -eq 0) {'Present'} else {'Absent'}}"
             DependsOn = "[Script]CloudWitness"
